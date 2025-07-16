3340a207d1095bfbfecf91d7638b10b3a9773b40
golurai205 <techgolu20@gmail.com>
2025-07-16 19:34:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateVideo() {
+  alert("🚀 Video generation started! (Coming Soon...)");
+}
+
+function login() {
+  alert("🔐 Login with Google (Coming Soon...)");
+}
+
+function logout() {
+  alert("🚪 Logout successful (Coming Soon...)");
+}
